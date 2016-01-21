8d04e5f738b5862ae6b1f6bc4c5639948e463d95
Ilya N. Zykin <zykin-ilya@ya.ru>
2016-01-21 13:23:04 +0300

    ENV VARS and SourceMaps

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,22 @@
 'use strict';
 
+const NODE_ENV = process.env.NODE_ENV || 'development';
+console.log(`NODE ENV => ${ NODE_ENV }`);
+
+const DEV_MODE = NODE_ENV == 'development';
+console.log(`DEV MODE => ${ DEV_MODE }`);
+
+// cheap-inline-module-source-map | source-map
+const SOURCE_MAP_TYPE = DEV_MODE ? 'source-map' : null;
+
 module.exports = {
-  watch: true,
+  watch: DEV_MODE,
 
   watchOptions: {
     aggregateTimeout: 100
   },
 
-  // cheap-inline-module-source-map
-  devtool: "source-map",
+  devtool: SOURCE_MAP_TYPE,
 
   // the base path which will be used to resolve entry points
   context: __dirname + '/__src/assets',
